1aa163d77f043ad482c18a381aaa6e6824ef1363
Can Koyuncu <reyyadvan@gmail.com>
2025-05-29 19:57:06 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,177 @@
+# Python text-based animation of CaSoDiRuHTra versus non CaSoDiRuHTra traffic flow
+from os import system
+from time import sleep
+def stdout(x):
+    system("cls")
+    print(x)
+    sleep(5)
+print("CaSoDiRuHTra vs Non-CaSoDiRuHTra")
+system("pause")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+⬜⬜⬜
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+🟥🟥🟥
+🟥🟥🟥
+🟥🟥🟥
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+
+🟥 = Car
+⬜ = Empty Road
+""")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+🟥🟥🟥
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+
+🟥 = Car
+⬜ = Empty Road
+""")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+⬜⬜⬜
+
+🟥 = Car
+⬜ = Empty Road
+""")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+🟥🟥🟥
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+
+🟥 = Car
+⬜ = Empty Road
+""")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+🟥🟥🟥
+🟥🟥🟥
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜ 
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+
+🟥 = Car
+⬜ = Empty Road
+""")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+🟥🟥🟥
+🟥🟥🟥
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+⬜⬜⬜
+🟥🟥🟥
+🟥🟥🟥
+
+🟥 = Car
+⬜ = Empty Road
+""")
+stdout("""
+CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+🟥🟥🟥
+🟥🟥🟥
+
+Non-CaSoDiRuHTra traffic (3 lanes and 3 cars per lane):
+
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+⬜⬜⬜
+🟥🟥🟥
+🟥🟥🟥
+🟥🟥🟥
+
+🟥 = Car
+⬜ = Empty Road
+""")
